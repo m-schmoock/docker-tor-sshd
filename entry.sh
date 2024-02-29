2396866a764bf41f37e2c5e6d15b0a8b36cd34e1
Michael Schmoock <michael@schmoock.net>
2024-02-29 11:19:42 +0100

    chore: optimized startup log output

--- entry.sh
+++ entry.sh
@@ -8,7 +8,9 @@ if grep -q ^root:*: /etc/shadow; then
 	echo "Setting random password for user root"
 	RND=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
 	echo root:$RND | chpasswd
-	echo "Random password for root :: $RND"
+	echo
+	echo "Random password for root user: $RND"
+	echo
 fi
 
 rm -f /var/log/tor/notices.log
@@ -23,17 +25,18 @@ do
   sleep 1
   echo -n .
 done
-
 echo " Done."
+
 echo
 echo "Add this to your ~/.ssh/config:"
 echo
-echo "    Host *.onion"
-echo "        ProxyCommand torsocks nc %h %p"
+echo "	Host *.onion"
+echo "		ProxyCommand torsocks nc %h %p"
 echo
 echo
 echo "Conenct using the generated password shown above via:>  ssh root@$(cat /var/lib/tor/hidden_service/hostname)"
 echo "Use this to copy your ssh pubkey:>  ssh-copy-id root@$(cat /var/lib/tor/hidden_service/hostname)"
 echo "Feel free to use local port forwarding to access web GUIs, e.g. via:> ssh -F 8080:192.168.0.123:80 ..."
+echo
 
 exec "$@"
